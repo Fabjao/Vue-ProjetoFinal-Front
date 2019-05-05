56fa67564ce499d7d1762793070cf42a1b15e05d
Fábio Gonçalves <fabiobasquete4@hotmail.com>
2019-05-05 00:26:30 -0300

    Criação de menu

--- src/modules/Dashboard/components/AppLayout.vue
+++ src/modules/Dashboard/components/AppLayout.vue
@@ -1,12 +1,22 @@
 <template>
-  <AppToolbar />
+  <div>
+    <AppToolbar v-model="drawer" />
+    <AppMenu v-model="drawer" />
+  </div>
+
 </template>
 <script>
+import AppMenu from './AppMenu.vue'
 import AppToolbar from './AppToolbar.vue'
+
 export default {
   name: 'AppLayout',
   components: {
-    AppToolbar
-  }
+    AppToolbar,
+    AppMenu
+  },
+  data: () => ({
+    drawer: false
+  })
 }
 </script>
