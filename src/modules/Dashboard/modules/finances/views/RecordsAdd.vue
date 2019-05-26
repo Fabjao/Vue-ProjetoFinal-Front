eaf2e2346b61029dfc38d2be376b30fcea9020de
Fábio Gonçalves <fabiobasquete4@hotmail.com>
2019-05-26 12:33:05 -0300

    Projeto final até aula 458

--- src/modules/Dashboard/modules/finances/views/RecordsAdd.vue
+++ src/modules/Dashboard/modules/finances/views/RecordsAdd.vue
@@ -1,61 +1,205 @@
 <template>
   <v-container text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs12 sm6 md4 lg4>
-        <p>Amount</p>
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex
+        xs12
+        sm6
+        md4
+        lg4
+      >
+        <NumericDisplay
+          :color="color"
+          v-model="$v.record.amount.$model"
+        />
       </v-flex>
-      <v-flex xs12 sm6 md8 lg8>
+      <v-flex
+        xs12
+        sm6
+        md8
+        lg8
+      >
         <v-card>
           <v-card-text>
             <v-form>
-              <v-select name="account" label="Conta" prepend-icon="account_balance" :items="accounts"></v-select>
-              <v-select name="category" label="Categoria" prepend-icon="class" :items="categories"></v-select>
+              <v-dialog
+                ref="dateDialog"
+                :return-value.sync="record.date"
+                v-model="showDateDialog"
+                persistent
+                lazy
+                width="290px"
+                full-width
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    name="date"
+                    label="Vencimento"
+                    prepend-icon="event"
+                    type="text"
+                    readonly
+                    :value="formattedDate"
+                    v-on="on"
+                  >
+                  </v-text-field>
+                </template>
+
+                <v-date-picker
+                  :color="color"
+                  locale="pt-br"
+                  scrollable
+                  v-model="dateDialogValue"
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    flat
+                    :color="color"
+                    @click="cancelarDialog"
+                  >
+                    Cancelar
+                  </v-btn>
+
+                  <v-btn
+                    flat
+                    :color="color"
+                    @click="$refs.dateDialog.save(dateDialogValue)"
+                  >Ok</v-btn>
+
+                </v-date-picker>
+              </v-dialog>
+
+              <v-select
+                name="account"
+                label="Conta"
+                prepend-icon="account_balance"
+                :items="accounts"
+                item-text="description"
+                item-value="id"
+                v-model="$v.record.accountId.$model"
+              ></v-select>
+              <v-select
+                name="category"
+                label="Categoria"
+                prepend-icon="class"
+                :items="categories"
+                item-text="description"
+                item-value="id"
+                v-model="$v.record.categoryId.$model"
+              ></v-select>
               <v-text-field
                 name="description"
                 label="Descrição"
                 prepend-icon="description"
                 type="text"
+                v-model.trim="$v.record.description.$model"
               ></v-text-field>
               <v-text-field
                 name="tags"
                 label="Tags (separadas por vírgula)"
                 prepend-icon="label"
                 type="text"
+                v-model="record.tags"
+                v-show="showTagsInput"
               ></v-text-field>
-              <v-text-field name="note" label="Observações" prepend-icon="note" type="text"></v-text-field>
+              <v-text-field
+                name="note"
+                label="Observações"
+                prepend-icon="note"
+                type="text"
+                v-model.trim="record.note"
+                v-show="showNoteInput"
+              ></v-text-field>
             </v-form>
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  small
+                  class="mr-3"
+                  v-on="on"
+                  @click="showTagsInput = !showTagsInput"
+                >
+                  <v-icon :color="color">label</v-icon>
+                </v-btn>
+              </template>
+              <span>Adicionar tags</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  small
+                  class="mr-3"
+                  v-on="on"
+                  @click="showNoteInput = !showNoteInput"
+                >
+                  <v-icon :color="color">label</v-icon>
+                </v-btn>
+              </template>
+              <span>Observação</span>
+            </v-tooltip>
           </v-card-text>
         </v-card>
+        <v-btn
+          color="secondary"
+          large
+          fab
+          class="mt-4"
+          @click="$router.back()"
+        >
+          <v-icon>close</v-icon>
+        </v-btn>
+
+        <v-btn
+          :color="color"
+          large
+          fab
+          class="mt-4"
+          @click="submit"
+        >
+          <v-icon>check</v-icon>
+        </v-btn>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapActions } from "vuex";
-import moment from "moment";
-import { decimal, minLength, required } from "vuelidate/lib/validators";
+import { mapActions } from 'vuex'
+import moment from 'moment'
+import { decimal, minLength, required } from 'vuelidate/lib/validators'
 
-import AccountService from "./../services/accounts-service";
-import CategoriesService from "./../services/categories-services";
+import AccountService from './../services/accounts-service'
+import CategoriesService from './../services/categories-services'
+
+import NumericDisplay from './../components/NumericDisplay.vue'
 
 export default {
-  name: "RecordsAdd",
-  data() {
+  name: 'RecordsAdd',
+  components: {
+    NumericDisplay
+  },
+  data () {
     return {
       accounts: [],
       categories: [],
+      dateDialogValue: moment().format('YYYY-MM-DD'),
       record: {
         type: this.$route.query.type.toUpperCase(),
         amount: 0,
-        date: moment().format("YYYY-MM-DD"),
-        accountId: "",
-        categoryId: "",
-        description: "",
-        tags: "",
-        note: ""
-      }
-    };
+        date: moment().format('YYYY-MM-DD'),
+        accountId: '',
+        categoryId: '',
+        description: '',
+        tags: '',
+        note: ''
+      },
+      showDateDialog: false,
+      showNoteInput: false,
+      showTagsInput: false
+    }
   },
   validations: {
     record: {
@@ -67,40 +211,58 @@ export default {
       description: { required, minLength: minLength(6) }
     }
   },
-  async created() {
-    this.changeTitle(this.$route.query.type);
-    this.accounts = await AccountService.accounts();
-    this.categories = await CategoriesService.categories({
-      oepration: this.$route.query.type
-    });
+  computed: {
+    color () {
+      switch (this.record.type) {
+        case 'CREDIT':
+          return 'primary'
+        case 'DEBIT':
+          return 'error'
+        default:
+          return 'primary'
+      }
+    },
+    formattedDate () {
+      return moment(this.record.date).format('DD/MM/YYYY')
+    }
   },
-  async beforeRouteUpdate(to, from, next) {
-    const { type } = to.query;
-    this.changeTitle(type);
-    this.record.type = type.toUpperCase();
-    this.categories = await CategoriesService.categories({ oepration: type });
-    next();
+  async created () {
+    this.changeTitle(this.$route.query.type)
+    this.accounts = await AccountService.accounts()
+    this.categories = await CategoriesService.categories({
+      operation: this.$route.query.type.toUpperCase()
+    })
+  },
+  async beforeRouteUpdate (to, from, next) {
+    const { type } = to.query
+    this.changeTitle(type)
+    this.record.type = type.toUpperCase()
+    this.categories = await CategoriesService.categories({ oepration: type })
+    next()
   },
   methods: {
-    ...mapActions(["setTitle"]),
-    changeTitle(recordType) {
-      let title;
-      console.log("recordType ", recordType);
-      switch (recordType) {
-        case "credit":
-          title = "Nova Receita";
-          break;
-        case "debit":
-          title = "Nova Despesa";
-          break;
-        default:
-          title = "Novo Lançamento";
-      }
-      this.setTitle({ title });
+    ...mapActions(['setTitle']),
+    cancelarDialog () {
+      this.showDateDialog = false
+      this.dateDialogValue = this.record.date
     },
-    log() {
-      console.log("Form: ", this.record);
+    changeTitle (recordType) {
+      let title
+      switch (recordType) {
+        case 'credit':
+          title = 'Nova Receita'
+          break
+        case 'debit':
+          title = 'Nova Despesa'
+          break
+        default:
+          title = 'Novo Lançamento'
+      }
+      this.setTitle({ title })
+    },
+    submit () {
+      console.log('Form: ', this.record)
     }
   }
-};
+}
 </script>
